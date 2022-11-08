65bd2efd8b0be273aac253a4eeeae8410d8f7761
Shitesh Khaw <115025455+shiteshkhaw@users.noreply.github.com>
2022-11-08 21:48:16 +0530

    Create DictionaryInPython.py

---
+++ DictionaryInPython.py
@@ -0,0 +1,16 @@
+#Dictionary are created using curly Brackets.
+#Key and values are separated using colon.
+
+myDict={"youtube":"Spark shooter",#Here "youtube" is the key whereas the "Spark shooter" is the value of the key
+        "insta":"mr__spark__yt",
+        1:214}
+print(type(myDict))
+print(myDict["insta"])
+print(myDict["youtube"])
+print(myDict[1])
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+print(myDict.keys())
+print(myDict.values())
+print(myDict.items())
